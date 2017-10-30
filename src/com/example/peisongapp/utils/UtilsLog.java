42ad76768ce8455a24ce373da9c9223fcd62c283
edge89 <236768994@qq.com>
2017-10-30 20:45:20 +0800

    Update UtilsLog.java

--- src/com/example/peisongapp/utils/UtilsLog.java
+++ src/com/example/peisongapp/utils/UtilsLog.java
@@ -14,48 +14,10 @@ public class UtilsLog {
 	 * 是否开启日志
 	 */
 	public static boolean isTest = false;
-	public static boolean isLog = true;//是否保存log日志到本地目录/soufun/log
-	/**
-	 * 聊天接口
-	 */
-//	public static String url = "http://chat.client.3g.soufun.com/HttpConnection";//正式接口
-	public static String url = "http://testchat.client.3g.soufun.com/HttpConnection"; //新聊天测试接口
+	public static boolean isLog = true;
 	
-//	public static String UPLOAD_URL = "http://soufunapp.3g.soufun.com/http/";//图片和音频上传的正式接口 ChatPhoto
-	public static String UPLOAD_URL = "http://111.204.241.196:9082/http/";//图片和音频上传的测试接口 ChatPhoto
-	
-	/**
-	 * 请求接口
-	 */
-//	public static final String HTTP_HOST = "soufunapp.3g.soufun.com/http/"; //南北方服务器	
-//	public static final String HTTP_HOST_ESF = "soufunappesf.3g.soufun.com/http/"; //二手房接口
-//	public static final String HTTP_HOST_ZF = "soufunappzf.3g.soufun.com/http/"; //租房接口
-	
-	public static final String HTTP_HOST = "111.204.241.196:9082/http/";//4.16新测试接口(外网可访问)
-	public static final String HTTP_HOST_ESF = "111.204.241.196:9082/http/";//4.16新测试接口(外网可访问)
-	public static final String HTTP_HOST_ZF = "111.204.241.196:9082/http/";//4.16新测试接口(外网可访问)
 
-	/**
-	 * 论坛请求接口(正式)
-	 */
-//    public static final String HTTP_HOST_XF = "housejk.wap.soufun.com/"; // 正式接口
-
-	/**
-	 * 论坛请求接口(测试)
-	 */
-	 public static final String HTTP_HOST_XF = "111.204.241.171/"; //测式接口
-	 
-		/**
-		 *是否使用资讯测试地址
-		 */
-//		public static final boolean isTestNews = false;
-//		public static final String HTTP_NEWS_TEST = "http://m.test.soufun.com/";  //资讯测试地址
-//		public static final String HTTP_NEWS = "http://m.soufun.com/";            //资讯正式地址
-
-	/**
-	 * 论坛房贷计算器的接口
-	 */
-	public static final String HTTP_HOST_XF_COMPUTE = "newhousejkn.soufun.com/house/webinterface/"; // 正式接口
+	public static final String HTTP_HOST_XF_COMPUTE ;
 
 	public static void d(String key, String value) {
 		if (isTest) {
